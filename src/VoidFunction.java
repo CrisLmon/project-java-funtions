20f8ac71760967b1e7093b38dceaa74f0f69713e
Cristian <179995201+CrisLmon@users.noreply.github.com>
2025-05-13 19:03:34 -0500

    función sin parámetros y sin retorno implementada

---
+++ src/VoidFunction.java
@@ -0,0 +1,9 @@
+public class VoidFunction {
+    public static void main(String[] args) {    
+        salute();
+    }
+
+    public static void salute() {
+        System.out.println("Holi, mondo");
+    }
+}
